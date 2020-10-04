09f7166780311c03b1ba068e4e776bfef6261f2f
xinxiangbin <xin_xiangbin@163.com>
2020-10-04 15:37:43 +0800

    每日英语

--- js/http.js
+++ js/http.js
@@ -1,9 +1,12 @@
 var http = (function(){
   var baseUrl = '/api';
   var requestCount = 0;
-  var mask = mui.createMask(function(){ return false; });  //callback为用户点击蒙版时自动执行的回调；
-  mask[0].id = 'mask';
-  $(mask[0]).append('<span class="mui-icon mui-icon-spinner mui-spin"></span>')
+  if(typeof mui != 'undefined'){
+    var mask = mui.createMask(function(){ return false; });  //callback为用户点击蒙版时自动执行的回调；
+    mask[0].id = 'mask';
+    $(mask[0]).append('<span class="mui-icon mui-icon-spinner mui-spin"></span>')
+    
+  }
   
   function request(opt){
     requestCount++;
@@ -85,6 +88,25 @@ var http = (function(){
       }
     })
     return targetObj
+  }
+  
+  Date.prototype.format = function(format){
+  	 var o = {
+  	 "M+" : this.getMonth()+1, //month
+  	 "d+" : this.getDate(),    //day
+  	 "h+" : this.getHours(),   //hour
+  	 "m+" : this.getMinutes(), //minute
+  	 "s+" : this.getSeconds(), //second
+  	 "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+  	 "S" : this.getMilliseconds() //millisecond
+  	 }
+  	 if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+  	 (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+  	 for(var k in o)if(new RegExp("("+ k +")").test(format))
+  	 format = format.replace(RegExp.$1,
+  	 RegExp.$1.length==1 ? o[k] :
+  	 ("00"+ o[k]).substr((""+ o[k]).length));
+  	 return format;
   }
   
   return {
